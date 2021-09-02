f82338b32fb55b754e69274959d17eb5f4b23733
Silvia Garcia <garcia.s091@gmail.com>
2021-09-02 18:28:45 -0500

    Class-components

---
+++ src/containers/AppGeek.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Login from '../components/Login'
+import Register from '../components/Register'
+
+export default class AppGeek extends Component {
+    render() {
+        return (
+            <div>
+                <Login />
+            </div>
+        )
+    }
+}
